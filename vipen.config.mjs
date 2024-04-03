31f4d03db8cfe3b937bdb31ee17b7c738c05f011
eXory2024 <exory@anio.software>
2024-04-03 08:07:03 +0200

    Upgrade to new vipen version.

--- vipen.config.mjs
+++ vipen.config.mjs
@@ -1,4 +1,4 @@
-import {searchAndReplace, copyFile} from "vipen/processing"
+import {generateFromTemplate} from "vipen/autogenerate"
 
 const asyncToSync = {
 	"async function tryStat": "function tryStat",
@@ -11,8 +11,8 @@ export default {
 	realm: "js",
 	type: "package",
 
-	preprocessing: [
-		searchAndReplace("src/template.mjs", asyncToSync, "src/auto/sync.mjs"),
-		copyFile("src/template.mjs", "src/auto/async.mjs")
-	]
+	autogenerate: {
+		"sync.mjs": generateFromTemplate("src/template.mjs", asyncToSync),
+		"async.mjs": generateFromTemplate("src/template.mjs", {})
+	}
 }
